50cfcd0994c461e6ad485e34f38ee5a48631e126
Ishan Pandita <panditaishan193@gmail.com>
2026-02-13 23:56:28 +0530

    Switch to Brevo SMTP

--- core/settings.py
+++ core/settings.py
@@ -103,15 +103,15 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_HOST = "smtp-relay.brevo.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = "counselling.live@gmail.com"
+EMAIL_HOST_PASSWORD = os.environ.get("BREVO_SMTP_KEY")
 
 DEFAULT_FROM_EMAIL = "Free Counselling <counselling.live@gmail.com>"
+
 
 
 DEFAULT_FROM_EMAIL = "Free Counselling <counselling.live@gmail.com>"  # can still use your Gmail as from address
